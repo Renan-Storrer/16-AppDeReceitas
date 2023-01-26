5a0a6d143c46457f224708f967d06409ab9aceaa
Ana <anateixeirazevedo@gmail.com>
2023-01-26 16:37:17 -0300

    feat: renderiza email na tela de profile, requisitos 57 e 58

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,11 +3,13 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const email = JSON.parse(localStorage.getItem('user'));
+
   return (
     <div>
       <Header title="Profile" profile />
       <div>
-        <h4 data-testid="profile-email">Email</h4>
+        <h4 data-testid="profile-email">{ email.email }</h4>
         <button data-testid="profile-done-btn">Done Recipes</button>
         <button data-testid="profile-favorite-btn">Favorite Recipes</button>
         <button data-testid="profile-logout-btn">Logout</button>
