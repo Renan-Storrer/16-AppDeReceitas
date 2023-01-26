f9c12a5531d773255e1160a3cc2d44e083da1b1f
Ana <anateixeirazevedo@gmail.com>
2023-01-26 16:08:26 -0300

    feat: cria botões da tela de Profile, requisito 57

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,6 +6,12 @@ function Profile() {
   return (
     <div>
       <Header title="Profile" profile />
+      <div>
+        <h4 data-testid="profile-email">Email</h4>
+        <button data-testid="profile-done-btn">Done Recipes</button>
+        <button data-testid="profile-favorite-btn">Favorite Recipes</button>
+        <button data-testid="profile-logout-btn">Logout</button>
+      </div>
       <Footer title="Footer" />
     </div>
   );
