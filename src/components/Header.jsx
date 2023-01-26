b78738f86ca3c6704d8922e40e9f1fef62584667
joaopassone <joao.passone@gmail.com>
2023-01-26 10:43:06 -0300

    feat: Requisitos 8 e 9 - Implementa as funcionalidades dos botões de pesquisa e profile do header e cobertura de testes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header(props) {
   const { title, profile, search } = props;
+  const history = useHistory();
+  const [showSearchBar, setShowSearchBar] = useState(false);
+
+  const handleSearchButton = () => {
+    setShowSearchBar(!showSearchBar);
+  };
 
   return (
-    <div className="header-component">
+    <header>
       { profile && <img
         src={ profileIcon }
         alt="profile"
         data-testid="profile-top-btn"
+        role="presentation"
+        onClick={ () => history.push('/profile') }
       /> }
-      { search && <img
-        src={ searchIcon }
-        alt="search"
-        data-testid="search-top-btn"
-      /> }
+      { search
+        && (
+          <div>
+            <img
+              src={ searchIcon }
+              alt="search"
+              data-testid="search-top-btn"
+              role="presentation"
+              onClick={ handleSearchButton }
+            />
+            { showSearchBar && <input
+              type="text"
+              name="searchInput"
+              id="search-input"
+              data-testid="search-input"
+            /> }
+          </div>
+        ) }
       <h2 data-testid="page-title">{ title }</h2>
-    </div>
+    </header>
   );
 }
 
---
+++ src/tests/Header.test.js
@@ -0,0 +1,133 @@
+import React from 'react';
+
+import userEvent from '@testing-library/user-event';
+import { screen, act } from '@testing-library/react';
+
+import renderWithRouter from './renderWithRouter/renderWithRouter';
+import App from '../App';
+import Header from '../components/Header';
+
+const PROFILE_BUTTON = 'profile-top-btn';
+const SEARCH_BUTTON = 'search-top-btn';
+const PAGE_TITLE = 'page-title';
+
+describe('Testes do componente Header', () => {
+  it('Verifica que o Header possui as informações corretas nas páginas da aplicação', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/meals');
+    });
+
+    let profileIcon = screen.getByTestId(PROFILE_BUTTON);
+    let searchIcon = screen.getByTestId(SEARCH_BUTTON);
+    let title = screen.getByTestId(PAGE_TITLE);
+
+    expect(profileIcon).toBeInTheDocument();
+    expect(searchIcon).toBeInTheDocument();
+    expect(title).toHaveTextContent('Meals');
+
+    act(() => {
+      history.push('/drinks');
+    });
+
+    expect(profileIcon).toBeInTheDocument();
+    expect(searchIcon).toBeInTheDocument();
+    expect(title).toHaveTextContent('Drinks');
+
+    act(() => {
+      history.push('/meals/:id-da-receita');
+    });
+
+    expect(profileIcon).not.toBeInTheDocument();
+    expect(searchIcon).not.toBeInTheDocument();
+
+    act(() => {
+      history.push('/drinks/:id-da-receita');
+    });
+
+    expect(profileIcon).not.toBeInTheDocument();
+    expect(searchIcon).not.toBeInTheDocument();
+
+    act(() => {
+      history.push('/meals/:id-da-receita/in-progress');
+    });
+
+    expect(profileIcon).not.toBeInTheDocument();
+    expect(searchIcon).not.toBeInTheDocument();
+
+    act(() => {
+      history.push('/drinks/:id-da-receita/in-progress');
+    });
+
+    expect(profileIcon).not.toBeInTheDocument();
+    expect(searchIcon).not.toBeInTheDocument();
+
+    act(() => {
+      history.push('/profile');
+    });
+
+    profileIcon = screen.getByTestId(PROFILE_BUTTON);
+    searchIcon = screen.queryByTestId(SEARCH_BUTTON);
+    title = screen.getByTestId(PAGE_TITLE);
+
+    expect(profileIcon).toBeInTheDocument();
+    expect(searchIcon).not.toBeInTheDocument();
+    expect(title).toHaveTextContent('Profile');
+
+    act(() => {
+      history.push('/done-recipes');
+    });
+
+    profileIcon = screen.getByTestId(PROFILE_BUTTON);
+    searchIcon = screen.queryByTestId(SEARCH_BUTTON);
+    title = screen.getByTestId(PAGE_TITLE);
+
+    expect(profileIcon).toBeInTheDocument();
+    expect(searchIcon).not.toBeInTheDocument();
+    expect(title).toHaveTextContent('Done Recipes');
+
+    act(() => {
+      history.push('/favorite-recipes');
+    });
+
+    profileIcon = screen.getByTestId(PROFILE_BUTTON);
+    searchIcon = screen.queryByTestId(SEARCH_BUTTON);
+    title = screen.getByTestId(PAGE_TITLE);
+
+    expect(profileIcon).toBeInTheDocument();
+    expect(searchIcon).not.toBeInTheDocument();
+    expect(title).toHaveTextContent('Favorite Recipes');
+
+    act(() => {
+      history.push('/');
+    });
+
+    expect(profileIcon).not.toBeInTheDocument();
+    expect(searchIcon).not.toBeInTheDocument();
+  });
+
+  it('Verifica se o botão de profile leva à página correta', () => {
+    const { history } = renderWithRouter(<Header profile />);
+
+    const profileButton = screen.getByTestId(PROFILE_BUTTON);
+    userEvent.click(profileButton);
+
+    expect(history.location.pathname).toBe('/profile');
+  });
+
+  it('Verifica se o botão de busca funciona corretamente', () => {
+    renderWithRouter(<Header search />);
+
+    let searchBar = screen.queryByTestId('search-input');
+
+    expect(searchBar).not.toBeInTheDocument();
+
+    const searchButton = screen.getByTestId(SEARCH_BUTTON);
+    userEvent.click(searchButton);
+
+    searchBar = screen.getByTestId('search-input');
+
+    expect(searchBar).toBeInTheDocument();
+  });
+});
