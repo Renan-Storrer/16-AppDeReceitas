9d99615ce246de2b742f94af6ad556798e3bfd89
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2023-01-26 16:51:53 -0300

    feat: cria rota do clique do botao Done Recipes, req 60

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
   const email = JSON.parse(localStorage.getItem('user'));
 
+  const history = useHistory();
+
+  const handleDoneRecipes = () => {
+    history.push('/done-recipes');
+  };
+
   return (
     <div>
       <Header title="Profile" profile />
       <div>
         <h4 data-testid="profile-email">{ email.email }</h4>
-        <button data-testid="profile-done-btn">Done Recipes</button>
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+          onClick={ handleDoneRecipes }
+        >
+          Done Recipes
+        </button>
         <button data-testid="profile-favorite-btn">Favorite Recipes</button>
         <button data-testid="profile-logout-btn">Logout</button>
       </div>
