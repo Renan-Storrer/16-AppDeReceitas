7daa9c4e4d51dc3c9d49d45a8089fcc56c62e2bd
joaopassone <joao.passone@gmail.com>
2023-01-31 18:11:01 -0300

    feat: Requisito 25: Implementa as informações da tela de detalhes

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -8,27 +8,85 @@ function RecipeDetails(props) {
   const location = useLocation();
   const [url, setUrl] = useState('');
   const [recipe, setRecipe] = useState({});
+  const [category, setCategory] = useState('');
+  const [keyName, setKeyName] = useState('');
+  const YT = 32;
 
   const fetchApi = useCallback(async () => {
     const response = await fetch(url);
     const json = await response.json();
-    setRecipe(json);
-  }, [url]);
+    setRecipe(json[category][0]);
+  }, [url, category]);
 
   useEffect(() => {
     if (location.pathname.includes('meals')) {
       setUrl(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+      setCategory('meals');
+      setKeyName('Meal');
     } else {
       setUrl(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+      setCategory('drinks');
+      setKeyName('Drink');
     }
   }, [location.pathname, id]);
 
   useEffect(() => {
     if (url) fetchApi();
   }, [fetchApi, url]);
+  console.log(recipe);
+
+  const ingredients = [];
+
+  for (let i = 1; i < 100; i += 1) {
+    const ingredient = recipe[`strIngredient${i}`];
+    const measure = recipe[`strMeasure${i}`];
+    if (ingredient) {
+      ingredients.push({ ingredient, measure });
+    } else break;
+  }
 
   return (
-    <div>RecipeDetails</div>
+    <div>
+      <img
+        src={ recipe[`str${keyName}Thumb`] }
+        alt="recipe"
+        data-testid="recipe-photo"
+      />
+      <h2
+        data-testid="recipe-title"
+      >
+        { recipe[`str${keyName}`] }
+      </h2>
+      <h3
+        data-testid="recipe-category"
+      >
+        { recipe.strCategory }
+        { category === 'drinks' && <small>{ ` (${recipe.strAlcoholic})` }</small> }
+      </h3>
+      <ul>
+        { ingredients.map((ingredient, i) => (
+          <li
+            key={ i }
+            data-testid={ `${i}-ingredient-name-and-measure` }
+          >
+            <strong>{ingredient.measure}</strong>
+            { ingredient.measure && 'of ' }
+            {ingredient.ingredient}
+          </li>))}
+      </ul>
+      <p
+        data-testid="instructions"
+      >
+        { recipe.strInstructions }
+      </p>
+      { category === 'meals' && recipe.strYoutube && <iframe
+        title="recipe-video"
+        width="420"
+        height="315"
+        src={ `https://www.youtube.com/embed/${recipe?.strYoutube.slice(YT)}` }
+        data-testid="video"
+      /> }
+    </div>
   );
 }
 
