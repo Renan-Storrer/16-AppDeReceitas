067dc0c0ebbe201d880dc2b0f6122e2fe7a87d78
joaopassone <joao.passone@gmail.com>
2023-01-31 16:17:42 -0300

    feat: Requisito 24: Faz chamada a API correta

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 
-function RecipeDetails() {
+function RecipeDetails(props) {
+  const { match } = props;
+  const { id } = match.params;
+  const location = useLocation();
+  const [url, setUrl] = useState('');
+  const [recipe, setRecipe] = useState({});
+
+  const fetchApi = useCallback(async () => {
+    const response = await fetch(url);
+    const json = await response.json();
+    setRecipe(json);
+  }, [url]);
+
+  useEffect(() => {
+    if (location.pathname.includes('meals')) {
+      setUrl(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+    } else {
+      setUrl(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+    }
+  }, [location.pathname, id]);
+
+  useEffect(() => {
+    if (url) fetchApi();
+  }, [fetchApi, url]);
+
   return (
     <div>RecipeDetails</div>
   );
 }
+
+RecipeDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.string,
+  }),
+}.isRequired;
 
 export default RecipeDetails;
