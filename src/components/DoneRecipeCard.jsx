5479224a7d643f54574ad3b62eb4be80778b0cec
joaopassone <joao.passone@gmail.com>
2023-02-02 16:56:56 -0300

    feat: Requisito 47: Compartilha a receita feita

--- src/components/DoneRecipeCard.jsx
+++ src/components/DoneRecipeCard.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import shareIcon from '../images/shareIcon.svg';
 
+const copy = require('clipboard-copy');
+
 function DoneRecipeCard(props) {
+  const [shared, setShared] = useState(false);
+
   const { recipe, index } = props;
-  const { tags } = recipe;
+  const { id, tags } = recipe;
   const firstTags = tags.slice(0, 2);
 
   return (
@@ -36,10 +40,12 @@ function DoneRecipeCard(props) {
         alt="share"
         role="presentation"
         data-testid={ `${index}-horizontal-share-btn` }
-        // onClick={ () => {
-        //   copy('http://localhost:3000');
-        // } }
+        onClick={ () => {
+          copy(`http://localhost:3000/${recipe.type}s/${id}`);
+          setShared(true);
+        } }
       />
+      { shared && <small>Link copied!</small> }
       { firstTags.map((tag) => (
         <span
           key={ tag }
