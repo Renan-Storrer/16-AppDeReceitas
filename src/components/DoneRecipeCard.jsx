c373a48fc666a09c9e8681b3e8881df3ba3f6457
joaopassone <joao.passone@gmail.com>
2023-02-02 16:17:48 -0300

    feat: Requisito 44: Implementa os elementos da tela de receitas feitas

---
+++ src/components/DoneRecipeCard.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import shareIcon from '../images/shareIcon.svg';
+
+function DoneRecipeCard(props) {
+  const { recipe, index } = props;
+  const { tags } = recipe;
+
+  return (
+    <div>
+      <img
+        src={ recipe.image }
+        alt={ `done-recipe-${index}` }
+        data-testid={ `${index}-horizontal-image` }
+      />
+      <h3
+        data-testid={ `${index}-horizontal-top-text` }
+      >
+        { recipe.category }
+      </h3>
+      <h3
+        data-testid={ `${index}-horizontal-name` }
+      >
+        { recipe.name }
+      </h3>
+      <h4
+        data-testid={ `${index}-horizontal-done-date` }
+      >
+        { recipe.doneDate }
+      </h4>
+      <img
+        src={ shareIcon }
+        alt="share"
+        role="presentation"
+        data-testid={ `${index}-horizontal-share-btn` }
+        onClick={ () => {
+          copy('http://localhost:3000');
+        } }
+      />
+      { tags.map((tag, i) => (
+        <span
+          key={ tag }
+          data-testid={ `${i}-${tag}-horizontal-tag` }
+        >
+          { tag }
+        </span>
+      ))}
+    </div>
+  );
+}
+
+DoneRecipeCard.propTypes = {
+  recipe: PropTypes.shape({}),
+  index: PropTypes.number,
+}.isRequired;
+
+export default DoneRecipeCard;
--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import Header from '../components/Header';
 
+import DoneRecipeCard from '../components/DoneRecipeCard';
+
 function DoneRecipes() {
+  const doneRecipes = localStorage.getItem('doneRecipes') || [];
+
   return (
     <div>
       <Header title="Done Recipes" profile />
+      <div>
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+        >
+          All
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-meal-btn"
+        >
+          Meals
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+        >
+          Drinks
+        </button>
+      </div>
+      { doneRecipes.map((doneRecipe, i) => (<DoneRecipeCard
+        key={ `done-recipe-card-${i}` }
+        index={ i }
+        recipe={ doneRecipe }
+      />))}
     </div>
   );
 }
